661a549446e69e45c2bd9bb163fff6d7d2e0e490
Ivan Sanchez <sanchezrivan2001@gmail.com>
2025-01-15 17:05:39 -0300

    feat: clases sobre useState me quede en la hora 02:03:10

--- src/ListadoApp.jsx
+++ src/ListadoApp.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../src/styles.css';
+import { AgregarProducto } from './components/AgregarProducto';
+const Item = ({ nombre, precio, comprado }) => {
+  return (
+    <li>
+      {nombre} - ${precio} - {comprado ? "☑️" : "✖️"}
+    </li>
+  );
+};
+
 
 export const ListadoApp = () => {
+  const addTask = () => {
+    setArreglo([...arreglo,{nombre: "Pastel de Manzana", precio: 15000, comprado: true}]);
+  }
+  // Corrección en la declaración del array
+  const listaProductos = [
+    { nombre: "Pan", precio: 1500, comprado: true },
+    { nombre: "Cafe", precio: 1000, comprado: true },
+    { nombre: "Azucar", precio: 3000, comprado: true },
+    { nombre: "Arroz", precio: 4000, comprado: false },
+    { nombre: "Purina", precio: 600, comprado: false },
+  ];
+
+  // Estado inicial con useState
+  const [arreglo, setArreglo] = useState(listaProductos);
+
   return (
     <>
-    <h1>ListadoApp</h1>
-    <ol>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-    </ol>
+      <h1>Listado de Productos a comprar</h1>
+      <AgregarProducto></AgregarProducto>
+      <ul>
+        {arreglo.map((item) => (
+          <Item
+            key={item.nombre} // La clave para cada elemento
+            nombre={item.nombre}
+            precio={item.precio}
+            comprado={item.comprado}
+          />
+        ))}
+      </ul>
+      
     </>
-    
-  )
-}
+  );
+};
