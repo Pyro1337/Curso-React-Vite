5aa4fcd77d24cecfd8457c9758570f280a37a4db
Ivan Sanchez <sanchezrivan2001@gmail.com>
2025-01-27 16:43:30 -0300

    Se sube lo del fetchUsers

--- src/UserApp.jsx
+++ src/UserApp.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import {useState, useEffect} from 'react'
 export const UserApp = () => {
+  const [users, setUsers] = useState([])
+  const fetchUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    setUsers(data)
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, []);
   return (
-    <h1>Lista de Usuarios</h1>
+    <>
+      <h1>Lista de Usuarios</h1>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
   )
 }
